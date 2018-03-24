7b5baa3dfc18b1036a7f41748f8e43814e8fe3e4
徐钰焜 <xykhktk@163.com>
2018-03-24 12:55:53 +0800

    首页空白页面

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once __DIR__.'/helper.php';
 /*
 |--------------------------------------------------------------------------
 | Create The Application
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title','首页')
+
+@section('content')
+    <div>首页</div>
+@stop
