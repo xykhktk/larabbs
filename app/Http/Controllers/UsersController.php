84879c86566b043b7447839fd2a3a5bee6991fb3
徐钰焜 <xykhktk@163.com>
2018-03-25 13:23:35 +0800

    edit user

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,14 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 
 class UsersController extends Controller
 {
+    public function show(User $user){
+		    return view('user.show',compact('user'));
+    }
 
-	public function show(User $user){
-		return view('user.show',compact('user'));
-	}
+    public function edit(User $user){
+        return view('user.edit',compact('user'));
+    }
+
+    public function update(UserRequest $request,User $user){
+			$user->update($request->all());
+			return redirect()->route('users.show',$user->id)->with('success','编辑成功');
+		}
 
 
 }
---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Auth;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+          'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
+          'email' => 'required|email',
+          'introduction' => 'max:80',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.between' => '用户名必须介于3-5个字符之间',
+            'introduction.max' => '个人简介不能大于80字符'
+        ];
+
+    }
+}
